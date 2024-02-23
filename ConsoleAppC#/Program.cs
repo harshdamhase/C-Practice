f980b21046e90ecae9be7280946834a40d151d4b
harshdamhase <har@1234mhase>
2024-02-23 14:28:45 -0800

    Add project files.

---
+++ ConsoleAppC#/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppC_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int var = 10;
+            String input="harshda";
+            long x = 12.5;
+double 
+            Console.WriteLine("Hello world" + var);
+            Console.Write(input);
+            Console.ReadLine();
+        }
+    }
+}
